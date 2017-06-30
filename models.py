bca4b45ccf04f96c06e098a002ff57f272ff6da5
admin_home <admin@mail.com>
2017-06-30 23:20:39 +0300

    just commit

--- models.py
+++ models.py
@@ -13,21 +13,19 @@ sys.setdefaultencoding('utf-8')
 class Users:
     @staticmethod
     def get_or_create_user(telegram_nickname, chat_id):
-        storage = shelve.open(shelve_name)
-        if not 'users' in storage:
-            storage['users'] = {}
-            logging.getLogger('bot').info('DB created')
-        if telegram_nickname not in storage['users']:
-            storage['users'][telegram_nickname] = {'chat_id': chat_id, 'active': True}
-            logging.getLogger('bot').info('Added new user: @{} ({})'.format(telegram_nickname, chat_id))
-        storage.close()
-        return storage['users'][telegram_nickname]
+        with shelve.open(shelve_name) as storage:
+            if not 'users' in storage:
+                storage['users'] = {}
+                logging.getLogger('bot').info('DB created')
+            if telegram_nickname not in storage['users']:
+                storage['users'][telegram_nickname] = {'chat_id': chat_id, 'active': True}
+                logging.getLogger('bot').info('Added new user: @{} ({})'.format(telegram_nickname, chat_id))
+            return storage['users'][telegram_nickname]
 
     @staticmethod
     def get_active():
-        storage = shelve.open(shelve_name)
-        if 'users' in storage:
-            for user in storage['users']:
-                if storage['users'][user]['active']:
-                    yield storage['users'][user]
-        storage.close()
+        with shelve.open(shelve_name) as storage:
+            if 'users' in storage:
+                for user in storage['users']:
+                    if storage['users'][user]['active']:
+                        yield storage['users'][user]
