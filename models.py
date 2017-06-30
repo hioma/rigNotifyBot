667cfd86b1e23c80dae0f500976dd12cb2525a7e
admin_home <admin@mail.com>
2017-06-30 23:28:55 +0300

    just commit

--- models.py
+++ models.py
@@ -13,10 +13,13 @@ sys.setdefaultencoding('utf-8')
 class Users:
     @staticmethod
     def get_or_create_user(telegram_nickname, chat_id):
+        print 'here1'
         with shelve.open(shelve_name) as storage:
+            print 'here2'
             if 'users' not in storage:
                 storage['users'] = {}
                 logging.getLogger('bot').info('DB created')
+            print 'here3'
             if telegram_nickname not in storage['users']:
                 storage['users'][telegram_nickname] = {'chat_id': chat_id, 'active': True}
                 logging.getLogger('bot').info('Added new user: @{} ({})'.format(telegram_nickname, chat_id))
